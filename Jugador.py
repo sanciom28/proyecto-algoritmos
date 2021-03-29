81cbb396a629c5c991487c5506a7c54ed24a3c94
sanciom28 <59677058+sanciom28@users.noreply.github.com>
2021-03-29 17:10:50 -0400

    Add files via upload
    Beginning of project repo

---
+++ Jugador.py
@@ -0,0 +1,8 @@
+class Jugador:
+    
+    def __init__(self, username, psw, age, avatar, time):
+        self.username = username
+        self.psw = psw
+        self.age = age
+        self.avatar = avatar
+        self.time = time
